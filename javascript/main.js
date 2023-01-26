29b9527fecfb24b5ec6101a119935e3f5f665827
ElDav1d <dvivoes@yahoo.es>
2023-01-26 09:59:58 +0100

    Display canvas

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,18 @@
 // * GLOBAL VARIABLES
-
-
-
-
+const startBtnDOM = document.querySelector("#start-btn");
+const canvas = document.querySelector("#my-canvas");
+const splashScreenDOM = document.querySelector("#splash-screen");
 
 // * STATE MANAGEMENT FUNCTIONS
+function startGame() {
+  console.log("EXECUTING");
 
-
-
-
-
+//   1 change screen
+splashScreenDOM.style.display = 'none'
+canvas.style.display = 'block'
+//   2 create game
+//   3 init game
+}
 
 // * ADD EVENT LISTENERS
-
-
-
-
-
+startBtnDOM.addEventListener("click", startGame);
